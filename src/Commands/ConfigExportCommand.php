d32f678be40b7e129584b24e78dccd0e192c2056
Aaron Bauman <aaron@messageagency.com>
2021-02-24 14:42:49 -0500

    Debugged

--- src/Commands/ConfigExportCommand.php
+++ src/Commands/ConfigExportCommand.php
@@ -5,7 +5,6 @@ namespace Pantheon\TerminusConfigExport\Commands;
 use Pantheon\Terminus\Commands\Remote\SSHBaseCommand;
 use Pantheon\Terminus\Commands\WorkflowProcessingTrait;
 use Pantheon\Terminus\Exceptions\TerminusException;
-use Pantheon\Terminus\Exceptions\TerminusProcessException;
 use Pantheon\Terminus\Models\Workflow;
 use Pantheon\Terminus\Site\SiteAwareInterface;
 use Pantheon\Terminus\Site\SiteAwareTrait;
@@ -53,49 +52,60 @@ class ConfigExportCommand extends SSHBaseCommand implements SiteAwareInterface {
    *
    * @param string $site_env The site UUID or machine name and environment
    *   (<site>.<env>)
+   *
    * @option string $message Commit message
+   * @option int $timeout How long to wait for sub-jobs to complete, in seconds.
    * @usage <site.env> Exports and commits config to <site.env>.
    */
-  public function configExport($site_env, $options = ['message' => 'Config export']) {
+  public function configExport($site_env, $options = ['message' => 'Config export', 'timeout' => self::DEFAULT_WORKFLOW_TIMEOUT]) {
     $this->options = $options;
-    list($this->site, $this->env) = $this->getSiteEnv($site_env);
+    [$this->site, $this->env] = $this->getSiteEnv($site_env);
     $workflow = $this->setSftp();
     $this->drushCex($workflow);
-    $workflow = $this->envCommit(time());
+    $workflow = $this->envCommit();
     $this->setGit($workflow);
   }
 
-  protected function setGit(Workflow $workflow) {
-    if ($this->env->hasUncommittedChanges()) {
-      throw new TerminusException('Failed to commit changes.');
+  protected function setGit(Workflow $workflow = NULL) {
+    if ($workflow) {
+      $this->wait($workflow);
     }
-
-    $this->wait($workflow);
     try {
       $this->log()->notice('Changing connection mode back to git.');
       $workflow = $this->env->changeConnectionMode('git');
     }
     catch (TerminusException $e) {
       $message = $e->getMessage();
-      if (strpos($message, 'sftp') !== FALSE) {
+      if (strpos($message, 'git') !== FALSE) {
         $this->log()->notice($message);
         return;
       }
       throw $e;
     }
-    $this->processWorkflow($workflow);
-    $this->log()->notice($workflow->getMessage());
+    if ($workflow) {
+      $workflow = $this->processWorkflow($workflow);
+      if ($workflow) {
+        $this->log()->notice($workflow->getMessage());
+      }
+    }
   }
 
-  protected function envCommit($time) {
-    $change_count = count((array)$this->env->diffstat());
-    if ($change_count === 0) {
-      throw new TerminusException('There is no code to commit.');
+  protected function envCommit() {
+    $startTime = time();
+    while (count((array) $this->env->diffstat()) === 0) {
+      $this->log()
+        ->notice('Waiting for site config to be available on filesystem.');
+      if ($startTime + $this->options['timeout'] < time()) {
+        $this->log()
+          ->error('Failed to detect any pending changes');
+        return;
+      }
+      // Wait a bit, then spin some more
+      sleep(5);
     }
-    $this->waitForWorkflow($time);
     $this->log()->notice('Committing site configuration to version control.');
     $workflow = $this->processWorkflow($this->env->commitChanges($this->options['message']));
-    $this->log()->notice('Your code was committed.');
+    $this->log()->notice('Site config was committed.');
     return $workflow;
   }
 
@@ -104,7 +114,6 @@ class ConfigExportCommand extends SSHBaseCommand implements SiteAwareInterface {
       $this->wait($workflow);
     }
     $this->prepareEnvironment($this->site->id . '.' . $this->env->id);
-    $this->setProgressAllowed(true);
     $this->log()->notice('Exporting site configuration.');
     $this->executeCommand(['drush', 'cex', '-y']);
   }
@@ -113,7 +122,7 @@ class ConfigExportCommand extends SSHBaseCommand implements SiteAwareInterface {
     if (in_array($this->env->id, ['test', 'live',])) {
       throw new TerminusException(
         'Connection mode cannot be set on the {env} environment',
-        ['env' => $this->id,]
+        ['env' => $this->env->id]
       );
     }
     try {
@@ -128,109 +137,47 @@ class ConfigExportCommand extends SSHBaseCommand implements SiteAwareInterface {
       }
       throw $e;
     }
-    $workflow = $this->processWorkflow($workflow);
-    $this->log()->notice($workflow->getMessage());
-    return $workflow;
+
+    // Sometimes we get a null workflow in response.
+    if ($workflow) {
+      $workflow = $this->processWorkflow($workflow);
+      if ($workflow) {
+        $this->log()->notice($workflow->getMessage());
+      }
+      return $workflow;
+    }
   }
 
   protected function wait(Workflow $workflow) {
     $startWaiting = time();
-    while(true) {
+    while (TRUE) {
       $workflowCreationTime = $workflow->get('created_at');
       $workflowDescription = $workflow->get('description');
 
       if (($workflowCreationTime > $startWaiting)) {
-        $this->log()->notice("Workflow '{current}' {status}.", ['current' => $workflowDescription, 'status' => $workflow->getStatus(), ]);
+        $this->log()
+          ->notice("Workflow '{current}' {status}.", [
+            'current' => $workflowDescription,
+            'status' => $workflow->getStatus(),
+          ]);
         if ($workflow->isSuccessful()) {
           $this->log()->notice("Workflow succeeded");
           return;
         }
       }
       else {
-        $this->log()->notice("Waiting for '{current}'", ['current' => $workflowDescription]);
+        $this->log()
+          ->notice("Waiting for '{current}'", ['current' => $workflowDescription]);
       }
       // Wait a bit, then spin some more
       sleep(1);
 
       if (time() - $startWaiting >= self::DEFAULT_WORKFLOW_TIMEOUT) {
-        $this->log()->warning("Waited '{max}' seconds, giving up waiting for workflow to finish", ['max' => self::DEFAULT_WORKFLOW_TIMEOUT]);
+        $this->log()
+          ->warning("Waited '{max}' seconds, giving up waiting for workflow to finish", ['max' => self::DEFAULT_WORKFLOW_TIMEOUT]);
         break;
       }
     }
-  }
-
-  /**
-   * Wait for a workflow to complete.
-   *
-   * @param int $startTime Ignore any workflows that started before the start time.
-   * @param string $workflow The workflow message to wait for.
-   */
-  protected function waitForWorkflow($startTime, $expectedWorkflowDescription = '', $maxWaitInSeconds = null)
-  {
-    if (empty($expectedWorkflowDescription)) {
-      $expectedWorkflowDescription = "Sync code on " . $this->env->id;
-    }
-
-    if (null === $maxWaitInSeconds) {
-      $maxWaitInSecondsEnv = getenv('TERMINUS_BUILD_TOOLS_WORKFLOW_TIMEOUT');
-      $maxWaitInSeconds = $maxWaitInSecondsEnv ? $maxWaitInSecondsEnv : self::DEFAULT_WORKFLOW_TIMEOUT;
-    }
-
-    $startWaiting = time();
-    while(true) {
-      $workflow = $this->getLatestWorkflow();
-      $workflowCreationTime = $workflow->get('created_at');
-      $workflowDescription = $workflow->get('description');
-
-      if (($workflowCreationTime > $startTime) && ($expectedWorkflowDescription == $workflowDescription)) {
-        $this->log()->notice("Workflow '{current}' {status}.", ['current' => $workflowDescription, 'status' => $workflow->getStatus(), ]);
-        if ($workflow->isSuccessful()) {
-          $this->log()->notice("Workflow succeeded");
-          return;
-        }
-      }
-      else {
-        $this->log()->notice("Current workflow is '{current}'; waiting for '{expected}'", ['current' => $workflowDescription, 'expected' => $expectedWorkflowDescription]);
-      }
-      // Wait a bit, then spin some more
-      sleep(5);
-
-      if (time() - $startWaiting >= $maxWaitInSeconds) {
-        $this->log()->warning("Waited '{max}' seconds, giving up waiting for workflow to finish", ['max' => $maxWaitInSeconds]);
-        break;
-      }
-    }
-  }
-
-  /**
-   * Fetch the info about the currently-executing (or most recently completed)
-   * workflow operation.
-   */
-  protected function getLatestWorkflow()
-  {
-    $workflows = $this->site->getWorkflows()->fetch(['paged' => false,])->all();
-    $workflow = array_shift($workflows);
-    $workflow->fetchWithLogs();
-    return $workflow;
-  }
-
-  /**
-   * Return the first item of the $command_args that is not an option.
-   *
-   * @param array $command_args
-   * @return string
-   */
-  private function firstArguments($command_args)
-  {
-    $result = '';
-    while (!empty($command_args)) {
-      $first = array_shift($command_args);
-      if (strlen($first) && $first[0] == '-') {
-        return $result;
-      }
-      $result .= " $first";
-    }
-    return $result;
   }
 
 }
